97a6c9636a7ff297be4773369f11d2aa015d3095
Oscar Aguilar <oscaraguilarlopez02@gmail.com>
2021-04-17 10:59:20 -0500

    Added sessions for auth user

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 const qrcode = require("qrcode-terminal");
 
+const fs = require("fs");
 const { Client } = require("whatsapp-web.js");
-const client = new Client();
+
+// Path where the session data will be stored
+const SESSION_FILE_PATH = "./session.json";
+
+// Load the session data if it has been previously saved
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  session: sessionData,
+});
+
+// Save session values to the file upon successful auth
+client.on("authenticated", (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
+});
 
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
@@ -14,7 +37,6 @@ client.on("ready", () => {
 client.on("message", (message) => {
   msg = message.body;
   if (msg.includes("!ping")) {
-    console.log(message.body);
     client.sendMessage(message.from, "pong");
   }
 });
